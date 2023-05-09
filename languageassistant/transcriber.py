796874fb53ee210dea3a80839ca1af0f13613ca7
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-05-09 12:40:06 -0400

    Modularize transcriber functionality

--- languageassistant/transcriber.py
+++ languageassistant/transcriber.py
@@ -4,32 +4,14 @@ from datetime import datetime, timedelta
 from queue import Queue
 from tempfile import NamedTemporaryFile
 from time import sleep
-from typing import Any, List, Optional
+from typing import Any, Optional
 
 import openai
 import speech_recognition as sr
-from pydantic import BaseModel, Extra
 
 
 def load_openai() -> None:
-    try:
-        import openai
-
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-    except ImportError:
-        raise ImportError(
-            "Package `openai` is not installed"
-            "Please install it with `pip install openai`"
-        )
-
-
-def print_transcription(transcription: List[str]) -> None:
-    # Clear the console to reprint the updated transcription.
-    os.system("cls" if os.name == "nt" else "clear")
-    for line in transcription:
-        print(line)
-    # Flush stdout.
-    print("", end="", flush=True)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def get_transcription(temp_file: str, wav_data: io.BytesIO) -> str:
@@ -43,16 +25,46 @@ def get_transcription(temp_file: str, wav_data: io.BytesIO) -> str:
     return text
 
 
-class Transcriber(BaseModel, extra=Extra.forbid):
+class Transcriber:
     """Transcription model for user input."""
 
-    record_timeout: Optional[float] = 2
-    """How real-time the recording is."""
-    phrase_timeout: Optional[float] = 3
-    """Delay before ending user monologue"""
-    energy_threshold: Optional[int] = 1000
-    """Microphone """
-    default_microphone: Optional[str] = "Microphone (2- Blue Snowball )"
+    def __init__(self, default_microphone: str = "list") -> None:
+        load_openai()
+        # Microphone Settings
+        self.record_timeout: float = 2
+        """How real-time the recording is."""
+        self.phrase_timeout: float = 3
+        """Delay before ending user monologue."""
+        self.energy_threshold: int = 1000
+        """Microphone energy threshold for background noise."""
+        self.default_microphone: str = default_microphone
+        """Default microphone name."""
+
+        # Recording variables
+        self.phrase_time: Optional[datetime] = None
+        """The last time a recording was retreived from the queue."""
+        self.last_sample: bytes = b""
+        """Current raw audio bytes."""
+        self.data_queue: Queue = Queue()
+        """Thread-safe Queue for passing data from the threaded recording callback."""
+        self.recorder: sr.Recognizer = sr.Recognizer()
+        """SpeechRecognizer to record and detect when speech ends."""
+        self.temp_file: str = NamedTemporaryFile(suffix=".wav").name
+        """File for sending to OpenAI Whisper API."""
+        self.transcription: str = ""
+        """Current transcription."""
+
+        # Recording settings
+        self.recorder.energy_threshold = self.energy_threshold
+        self.recorder.dynamic_energy_threshold = False
+        self.source: sr.Microphone = self.get_microphone()
+        with self.source as source:
+            self.recorder.adjust_for_ambient_noise(source)
+        # Create a background thread that will pass us raw audio bytes.
+        self.recorder.listen_in_background(
+            self.source, self._record_callback, phrase_time_limit=self.record_timeout
+        )
+        print("Transcription ready.")
 
     def get_microphone(self) -> sr.Microphone:
         """Get speech recognition microphone device"""
@@ -73,90 +85,63 @@ class Transcriber(BaseModel, extra=Extra.forbid):
                 return sr.Microphone(sample_rate=16000, device_index=index)
         raise KeyError("No microphone with name", mic_name)
 
-    def run(self) -> None:
-        load_openai()
-        # The last time a recording was retreived from the queue.
-        phrase_time = None
-        # Current raw audio bytes.
-        last_sample = b""
-        # Thread safe Queue for passing data from the threaded recording callback.
-        data_queue = Queue()  # type: Queue
-        # SpeechRecognizer to record because it can detect when speech ends.
-        recorder = sr.Recognizer()
-        recorder.energy_threshold = self.energy_threshold
-        recorder.dynamic_energy_threshold = False
-        record_timeout = self.record_timeout
-        phrase_timeout = self.phrase_timeout
+    def _record_callback(self, _: Any, audio: sr.AudioData) -> None:
+        """
+        Threaded callback function to recieve audio data when recordings finish.
+        audio: An AudioData containing the recorded bytes.
+        """
+        # Grab the raw bytes and push it into the thread safe queue.
+        data = audio.get_raw_data()
+        self.data_queue.put(data)
 
-        temp_file = NamedTemporaryFile(suffix=".wav").name
-        transcription = [""]
+    def run(self) -> Optional[str]:
+        # assert self.source is not None, 'Invalid microphone source'
+        now = datetime.utcnow()
+        # Pull raw recorded audio from the queue.
+        if not self.data_queue.empty():
+            phrase_complete = False
+            # If enough time has passed between recordings, consider the phrase complete.
+            # Clear the current working audio buffer to start over with the new data.
+            if self.phrase_time and now - self.phrase_time > timedelta(
+                seconds=self.phrase_timeout
+            ):
+                self.last_sample = b""
+                phrase_complete = True
+            # This is the last time we received new audio data from the queue.
+            self.phrase_time = now
 
-        def record_callback(_: Any, audio: sr.AudioData) -> None:
-            """
-            Threaded callback function to recieve audio data when recordings finish.
-            audio: An AudioData containing the recorded bytes.
-            """
-            # Grab the raw bytes and push it into the thread safe queue.
-            data = audio.get_raw_data()
-            data_queue.put(data)
+            # Concatenate our current audio data with the latest audio data.
+            while not self.data_queue.empty():
+                data = self.data_queue.get()
+                self.last_sample += data
 
-        with self.get_microphone() as source:
-            recorder.adjust_for_ambient_noise(source)
+            # Use AudioData to convert the raw data to wav data.
+            audio_data = sr.AudioData(
+                self.last_sample, self.source.SAMPLE_RATE, self.source.SAMPLE_WIDTH
+            )
+            wav_data = io.BytesIO(audio_data.get_wav_data())
+            text = get_transcription(self.temp_file, wav_data)
 
-        # Create a background thread that will pass us raw audio bytes.
-        recorder.listen_in_background(
-            source, record_callback, phrase_time_limit=record_timeout
-        )
-        print("Model loaded.\n")
-
-        while True:
-            try:
-                now = datetime.utcnow()
-                # Pull raw recorded audio from the queue.
-                if not data_queue.empty():
-                    phrase_complete = False
-                    # If enough time has passed between recordings, consider the phrase complete.
-                    # Clear the current working audio buffer to start over with the new data.
-                    if phrase_time and now - phrase_time > timedelta(
-                        seconds=phrase_timeout
-                    ):
-                        last_sample = b""
-                        phrase_complete = True
-                    # This is the last time we received new audio data from the queue.
-                    phrase_time = now
-
-                    # Concatenate our current audio data with the latest audio data.
-                    while not data_queue.empty():
-                        data = data_queue.get()
-                        last_sample += data
-
-                    # Use AudioData to convert the raw data to wav data.
-                    audio_data = sr.AudioData(
-                        last_sample, source.SAMPLE_RATE, source.SAMPLE_WIDTH
-                    )
-                    wav_data = io.BytesIO(audio_data.get_wav_data())
-                    text = get_transcription(temp_file, wav_data)
-
-                    # If we detected a pause between recordings, add a new item to our transcripion.
-                    # Otherwise edit the existing one.
-                    if phrase_complete:
-                        transcription.append(text)
-                    else:
-                        transcription[-1] = text
-
-                    print_transcription(transcription)
-                    sleep(0.25)
-            except KeyboardInterrupt:
-                break
-
-        print("\n\nTranscription:")
-        for line in transcription:
-            print(line)
+            # If we detected a pause between recordings, add a new item to our transcription.
+            # Otherwise edit the existing one.
+            self.transcription = text
+            if phrase_complete:
+                return self.transcription
+            print("\r" + self.transcription, end="")
+            return self.run()
+        return None
 
 
 def main() -> None:
-    transcriber = Transcriber()
-    transcriber.run()
+    transcriber = Transcriber(default_microphone="Microphone (2- Blue Snowball )")
+    while True:
+        try:
+            transcription = transcriber.run()
+            if transcription:
+                print("\r" + transcription)
+            sleep(0.25)
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == "__main__":
