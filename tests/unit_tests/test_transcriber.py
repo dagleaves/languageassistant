11986fecb4fbd2e961c0edb36882d300076df7e6
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-05-09 12:51:12 -0400

    Add transcriber microphone tests

--- tests/unit_tests/test_transcriber.py
+++ tests/unit_tests/test_transcriber.py
@@ -1,6 +1,12 @@
-import openai
+import io
+from tempfile import NamedTemporaryFile
 
-from languageassistant.transcriber import Transcriber, load_openai
+import openai
+import pytest
+import speech_recognition as sr
+from pytest import MonkeyPatch
+
+from languageassistant.transcriber import Transcriber, get_transcription, load_openai
 
 
 def test_openai_apikey() -> None:
@@ -8,5 +14,35 @@ def test_openai_apikey() -> None:
     assert openai.api_key != ""
 
 
-def test_transcriber_initialization() -> None:
+def test_transcriber_initialization(monkeypatch: MonkeyPatch) -> None:
+    print(type(monkeypatch))
+    monkeypatch.setattr("builtins.input", lambda _: 1)
     Transcriber()
+
+
+def test_get_transcription_too_short() -> None:
+    temp_file = NamedTemporaryFile(suffix=".wav").name
+    audio_data = sr.AudioData(b"", 16000, 2)
+    wav_data = io.BytesIO(audio_data.get_wav_data())
+    with pytest.raises(openai.error.InvalidRequestError):
+        get_transcription(temp_file, wav_data)
+
+
+def test_get_transcription_null_bytes() -> None:
+    temp_file = NamedTemporaryFile(suffix=".wav").name
+    audio_bytes = bytes([0 for _ in range(3500)])
+    audio_data = sr.AudioData(audio_bytes, 16000, 2)
+    wav_data = io.BytesIO(audio_data.get_wav_data())
+    get_transcription(temp_file, wav_data)
+
+
+def test_get_microphone_invalid_name(monkeypatch: MonkeyPatch) -> None:
+    with pytest.raises(KeyError):
+        Transcriber(default_microphone="invalid")
+
+
+def test_transcriber_run_empty(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("builtins.input", lambda _: 1)
+    transcriber = Transcriber()
+    transcription = transcriber.run()
+    assert transcription is None
