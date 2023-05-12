ec6e8a969f5082bd0613206c02b3adc584bfec99
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-05-12 18:40:05 -0400

    Fix missing openai api key skip

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -5,7 +5,10 @@ import pytest
 
 from languageassistant.utils import load_openai_api_key
 
+invalid_openai_api_key = os.getenv("OPENAI_API_KEY") in [None, "", "api_key"]
 
+
+@pytest.mark.skipif(invalid_openai_api_key, reason="Needs valid OpenAI API key")
 def test_openai_api_key_env_success() -> None:
     load_openai_api_key()
     assert openai.api_key != ""
