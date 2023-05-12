0c91f2bf5fdac49923875f8c7148da3779b0a9db
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-05-12 18:25:45 -0400

    Fix transcription test skips

--- tests/unit_tests/test_transcriber.py
+++ tests/unit_tests/test_transcriber.py
@@ -1,4 +1,5 @@
 import io
+import os
 from tempfile import NamedTemporaryFile
 
 import openai
@@ -8,16 +9,20 @@ from pytest import MonkeyPatch
 
 from languageassistant.transcriber import Transcriber, get_transcription
 
-microphones = sr.Microphone.list_microphone_names()
 
-
-@pytest.mark.skipif(not microphones, reason="No microphone available")
+@pytest.mark.skipif(
+    not sr.Microphone.list_microphone_names(), reason="No microphone available"
+)
 def test_transcriber_initialization(monkeypatch: MonkeyPatch) -> None:
     print(type(monkeypatch))
     monkeypatch.setattr("builtins.input", lambda _: 1)
     Transcriber()
 
 
+@pytest.mark.skipif(
+    os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "",
+    reason="Needs valid OpenAI API key",
+)
 def test_get_transcription_too_short() -> None:
     temp_file = NamedTemporaryFile(suffix=".wav").name
     audio_data = sr.AudioData(b"", 16000, 2)
@@ -26,6 +31,10 @@ def test_get_transcription_too_short() -> None:
         get_transcription(temp_file, wav_data)
 
 
+@pytest.mark.skipif(
+    os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "",
+    reason="Needs valid OpenAI API key",
+)
 def test_get_transcription_null_bytes() -> None:
     temp_file = NamedTemporaryFile(suffix=".wav").name
     audio_bytes = bytes([0 for _ in range(3500)])
