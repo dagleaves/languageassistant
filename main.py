0287f73c5e4d5e2c9b1b1c8e2644d346627216e6
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2023-06-01 13:20:32 -0400

    Integrate tts with assistant

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import questionary
 
 from languageassistant.assistant import Assistant
 from languageassistant.transcriber import Transcriber
+from languageassistant.tts import TTS
+from languageassistant.utils import supported_languages
 
 
 def main() -> None:
     language = questionary.select(
         "What language would you like to learn?",
-        choices=["English", "Spanish", "Chinese"],
+        choices=supported_languages,
     ).ask()
     proficiency = questionary.select(
         "What is your proficiency with this language?",
         choices=["Beginner", "Elementary", "Intermediate", "Advanced", "Fluent"],
     ).ask()
     transcriber = Transcriber()
+    tts = TTS(language=language)
     assistant = Assistant(
-        language=language, proficiency=proficiency, transcriber=transcriber
+        language=language, proficiency=proficiency, transcriber=transcriber, tts=tts
     )
     assistant.run()
 
